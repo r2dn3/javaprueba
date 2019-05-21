0c724c35051d97dad0439f242f7a3293079b4ffa
Jesus <Jesus@DESKTOP-DCJT21S>
2019-05-21 16:16:41 +0200

    Version 1.0 Muestra tu nombre.

---
+++ NameShower/src/Name.java
@@ -0,0 +1,12 @@
+
+public class Name {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		getName("Alicia");
+	}
+	public static String getName(String name) {
+		System.out.println("Tu nombre es " + name);
+		return name;
+	}
+
+}
